a558a0bbc390c25d9d948f2b3e54ee32dcd5525a
Himanshu Bhandary <2032ushimanshu@gmail.com>
2025-06-21 22:59:33 +0530

    Added notes in the bash script

---
+++ ATAC-seq/DownstreamAnalysisAndAnnotation/scaffold_structure.sh
@@ -0,0 +1,17 @@
+mkdir Benchmark
+touch ChIPseeker.smk
+touch config.yaml
+mkdir -p data/test_input; cd data; mkdir test_output
+touch Dockerfile
+mkdir envs; cd envs; touch ChIPseeeker.yaml
+touch Jenkinsfile
+mkdir -p logs/stderr; cd logs; mkdir  stdout
+touch run_chipseeker.R
+mkdir samples
+touch scaffold_structure.sh
+touch Snakefile 
+
+#chmod +x scaffold_structure.sh
+#./scaffold_structure.sh
+
+
