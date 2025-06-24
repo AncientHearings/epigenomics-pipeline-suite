4f03bc9e7d25ac1e3de50d103e8a37d75d1177bf
Himanshu Bhandary <2032ushimanshu@gmail.com>
2025-06-24 17:08:09 +0530

    Created a bash script to create the directory structure automatically

---
+++ ATAC-seq/PeakCallingAndSignalGeneration/directorystruture.sh
@@ -0,0 +1,13 @@
+mkdir generich 
+cd generich
+touch Dockerfile
+nano Dockerfile 
+
+
+#....chmod +x directorystructure.sh 
+  #To give executable permission to the file 
+#source ~/.bashrc
+  #To apply the changes
+  
+  
+
